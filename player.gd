429c03a8ce0bafdbe69cf03053e6cd6ec94c86e2
HiImCVH <greater.dog99@gmail.com>
2025-11-23 12:35:06 -0800

    Levelin time

--- player.gd
+++ player.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-const SHAPES = {"square","circle","triangle"}
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
