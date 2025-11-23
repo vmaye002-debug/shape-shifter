af702506b385c2f2b6e15e0e05d26d5ff90427c1
Ryan Situ <ryansitu120@gmail.com>
2025-11-23 14:49:47 -0800

    h

--- enemy_square.gd
+++ enemy_square.gd
@@ -1,19 +1,35 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-
 @export var areaDetect:Area2D
-var dropping:bool = false
-func _physics_process(delta: float) -> void:
-	
-	
+var startingY:float
+var returning:bool = false
 
-	move_and_slide()
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		returning = true
+		if collision.get_collider().name == "Player" and collision.get_collider().state_now == "T":
+			queue_free()
+			
+	if returning:
+		if position.y > startingY:
+			velocity.y = -400
+		else:
+			velocity.y = 0
+			position.y = startingY
+			returning = false
+		
+
+	#move_and_slide()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("dropping");
-	dropping = true;
-	#velocity.y = 500;
+	print(body)
+	velocity.y = 600;
+	pass # Replace with function body.
+
+
+func _on_area_2d_ready() -> void:
+	startingY = position.y
 	pass # Replace with function body.
