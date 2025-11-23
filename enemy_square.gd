dd0afad2ca155c82d5b776fdeb5ca9991f0f1296
Ryan Situ <ryansitu120@gmail.com>
2025-11-23 13:38:05 -0800

    jgtgpspf

--- enemy_square.gd
+++ enemy_square.gd
@@ -7,25 +7,24 @@ var returning:bool = false
 func _physics_process(delta: float) -> void:
 	if move_and_collide(velocity * delta):
 		returning = true
-	if returning:
-		if position.y > 300:
-			velocity.y = -400
-		else:
-			velocity.y = 0
-			position.y = 300
-			returning = false
+	#if returning:
+		#if position.y > 300:
+			#velocity.y = -400
+		#else:
+			#velocity.y = 0
+			#position.y = 300
+			#returning = false
+		print(position)
 	
-	
-
 	move_and_slide()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	dropping = true;
-	velocity.y = 500;
+	#velocity.y = 500;
+	print(body)
 	pass # Replace with function body.
 
 
 func _on_ready() -> void:
-	position.y = 300
 	pass # Replace with function body.
