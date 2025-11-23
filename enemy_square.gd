d7dfe72eb7b24376677f67cb75d7a907cfe38383
Ryan Situ <ryansitu120@gmail.com>
2025-11-23 13:13:24 -0800

    square drop and return

--- enemy_square.gd
+++ enemy_square.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-
 @export var areaDetect:Area2D
 var dropping:bool = false
+var returning:bool = false
 func _physics_process(delta: float) -> void:
+	if move_and_collide(velocity * delta):
+		returning = true
+	if returning:
+		if position.y > 300:
+			velocity.y = -400
+		else:
+			velocity.y = 0
+			position.y = 300
+			returning = false
 	
 	
 
@@ -13,7 +21,11 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("dropping");
 	dropping = true;
-	#velocity.y = 500;
+	velocity.y = 500;
+	pass # Replace with function body.
+
+
+func _on_ready() -> void:
+	position.y = 300
 	pass # Replace with function body.
